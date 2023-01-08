bb59dc1ca93b59a4014869e1426e165b4d4e9c81
동주 장 <ehdwngudwn@naver.com>
2023-01-08 14:35:56 +0900

    Managers, ResourceManager,구현

---
+++ Assets/Scripts/Managers/Core/Poolable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//오브젝트 풀링을 사용할 오브젝트에 추가하는 component
+public class Poolable : MonoBehaviour
+{
+    public bool IsUsing;
+}
---
+++ Assets/Scripts/Utils/Util.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    //go에 Component가 있으면 get 없다면 add하고 get
+    public static T GetOrAddComponent<T>(GameObject go) where T : UnityEngine.Component
+    {
+        T component = go.GetComponent<T>();
+        if (component == null)
+            component = go.AddComponent<T>();
+
+        return component;
+    }
+
+    //go의 자식 중 이름이 name인 자식 찾아서 해당 자식gameObject return
+    public static GameObject FindChild(GameObject go, string name = null, bool recursive = false)
+    {
+        Transform transform = FindChild<Transform>(go, name, recursive);
+        if (transform == null)
+            return null;
+
+        return transform.gameObject;
+    }
+
+    //go의 자식 중 이름이 name인 자식 찾아서 해당 component return, recursive : 자식의 자식까지 재귀적으로 찾을지
+    public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object
+    {
+        if (go == null)
+            return null;
+
+        if (recursive == false)
+        {
+            for(int i=0; i<go.transform.childCount; i++)
+            {
+                Transform transform = go.transform.GetChild(i);
+                if(string.IsNullOrEmpty(name) || transform.name == name)
+                {
+                    T component = transform.GetComponent<T>();
+                    if (component == null)
+                        return component;
+                }
+            }
+        }
+        else
+        {
+            foreach ( T component in go.GetComponentsInChildren<T>())
+            {
+                if (string.IsNullOrEmpty(name) || component.name == name)
+                    return component;
+            }
+        }
+        return null;
+    }
+}
