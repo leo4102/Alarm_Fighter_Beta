15a5838d557e26783d1aa827a19e27254e53d0d4
동주 장 <ehdwngudwn@naver.com>
2023-02-16 18:17:31 +0900

    modify playerAnim

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -69,6 +69,10 @@ public class Character : MonoBehaviour
 
     protected void Attack()
     {
+        WeaponInfo wp = GetComponent<WeaponInfo>();
+        if (wp.CurrentWeapon == null)
+            return;
 
+        wp.CurrentWeapon.Attack();
     }
 }
