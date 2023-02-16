329dabc0c8e9e7f0f808b0d0cd38563196d12a06
l3umblee <99014066+l3umblee@users.noreply.github.com>
2023-02-17 01:28:29 +0900

    Add HpUpdater, Stat

---
+++ Assets/Scripts/Stats/PlayerStat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : Stat
+{
+    void Start()
+    {
+        MaxHP = 10f;
+        CurrentHP = 10f;
+    }
+}
